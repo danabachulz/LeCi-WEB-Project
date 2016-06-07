17b8af9dc146510e9e7d8404b4d73986249481ad
jrsius <xave96@gmail.com>
2016-06-07 22:50:15 +0700

    Add files via upload

---
+++ panggilmahasiswajsp.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package executepart2;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author joni
+ */
+
+public class panggilmahasiswajsp extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        executepart2.query q=new executepart2.query();
+        HttpSession session = request.getSession(true);
+        String x=(String)session.getAttribute("nomx");
+        try{
+            ArrayList<MataKuliah> mk =q.makul();
+            ArrayList<Kelas> kl=q.keles();
+            ArrayList<Mahasiswa> mhs=q.nimmsh(x);
+            request.setAttribute("makul",mk);
+            request.setAttribute("keles", kl);
+            request.setAttribute("nimmsh", mhs);
+            q.closeConn();
+            request.getRequestDispatcher("/sudahloginmhs.jsp").forward(request, response);
+        }
+        catch(SQLException ex){
+            Logger.getLogger(panggil.class.getName()).log(Level.SEVERE,null,ex);
+        } catch (Exception ex) {
+            Logger.getLogger(panggilmahasiswajsp.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}

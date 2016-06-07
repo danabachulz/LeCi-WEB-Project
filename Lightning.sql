f96737ee7e8f1e8f1753fc81b0aa0881ab36536b
wayans <wayansazwidka@gmail.com>
2016-06-08 02:35:48 +0700

    Add files via upload

---
+++ Lightning.sql
@@ -0,0 +1,15 @@
+--------------------------------------------------------
+--  File created - Wednesday-June-08-2016   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table LIGHTNING
+--------------------------------------------------------
+
+  CREATE TABLE "HR"."LIGHTNING" 
+   (	"NIM" VARCHAR2(50 BYTE), 
+	"ISI" VARCHAR2(50 BYTE)
+   ) PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT)
+  TABLESPACE "USERS" ;
+REM INSERTING into HR.LIGHTNING

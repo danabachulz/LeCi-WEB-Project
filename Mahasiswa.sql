7145d2e8afa7ba4febe97af3a18a49bd98ea02fc
wayans <wayansazwidka@gmail.com>
2016-06-08 02:36:00 +0700

    Add files via upload

---
+++ Mahasiswa.sql
@@ -0,0 +1,90 @@
+--------------------------------------------------------
+--  File created - Wednesday-June-08-2016   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table MAHASISWA
+--------------------------------------------------------
+
+  CREATE TABLE "HR"."MAHASISWA" 
+   (	"NIM" NUMBER(10,0), 
+	"NAMA" VARCHAR2(50 BYTE), 
+	"PRODI" VARCHAR2(50 BYTE)
+   ) PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT)
+  TABLESPACE "USERS" ;
+REM INSERTING into HR.MAHASISWA
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314016,'Joni Rourensius','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314065,'Putu Dana','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314103,'Wayan Okta','Teknik Infromatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314001,'Raisa Andriana','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314002,'Pevita Pearce','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314003,'Isyana Sarasvati','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314004,'Tulus','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314005,'Raditya Dika','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314006,'Afgan Syahreza','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314007,'Ariana Grande','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314008,'Bruno Mars','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314009,'Iggy Azalea','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314010,'Lukas Graham','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314011,'Rihanna','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314012,'Avril Lavigne','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314013,'Katy Perry','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314014,'Shawn Mendes','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314015,'Zayn Malik','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314017,'Bon Jovi','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314018,'Avicii','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314019,'Zedd','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314020,'Martin Garrix','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314021,'Calvin Harris','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314022,'Charlie Puth','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314023,'Ellie Goulding','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314024,'Wiz Khalifa','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314025,'Adam Levine','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314026,'Alesso','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314027,'Beyonce Knowless','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314028,'Shakira','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314029,'Christina Perri','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314030,'David Guetta','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314031,'Jennifer Lopez','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314032,'Agnes Monica','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314033,'Justin Timberlake','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314034,'Sam Smith','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314035,'Miley Cyrus','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314036,'Omi','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314037,'Petra Sihombing','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314038,'Virzha','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314039,'Justin Bieber','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314040,'Madonna','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314041,'Bunga Citra Lestari','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314042,'Julia Perez','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314043,'Cita Citata','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314044,'Rafi Ahmad','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314045,'Nagita Slavina','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145134046,'Jessica Iskandar','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314047,'Keenan Pearce','Teknik Informatika');
+Insert into HR.MAHASISWA (NIM,NAMA,PRODI) values (145314048,'Anna Kendrick','Teknik Informatika');
+--------------------------------------------------------
+--  DDL for Index MAHASISWA_PK
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "HR"."MAHASISWA_PK" ON "HR"."MAHASISWA" ("NIM") 
+  PCTFREE 10 INITRANS 2 MAXTRANS 255 
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT)
+  TABLESPACE "USERS" ;
+--------------------------------------------------------
+--  Constraints for Table MAHASISWA
+--------------------------------------------------------
+
+  ALTER TABLE "HR"."MAHASISWA" ADD CONSTRAINT "MAHASISWA_PK" PRIMARY KEY ("NIM")
+  USING INDEX PCTFREE 10 INITRANS 2 MAXTRANS 255 
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT)
+  TABLESPACE "USERS"  ENABLE;
+ 
+  ALTER TABLE "HR"."MAHASISWA" MODIFY ("NIM" NOT NULL ENABLE);
+ 
+  ALTER TABLE "HR"."MAHASISWA" MODIFY ("NAMA" NOT NULL ENABLE);
+ 
+  ALTER TABLE "HR"."MAHASISWA" MODIFY ("PRODI" NOT NULL ENABLE);

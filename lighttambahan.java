d387eb8c6b6620b7c6929b838203b80fa70ba1e8
jrsius <xave96@gmail.com>
2016-06-08 12:10:15 +0700

    Add files via upload

---
+++ lighttambahan.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package executepart2;
+
+public class lighttambahan {
+    private String nim,isi;
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getIsi() {
+        return isi;
+    }
+
+    public String setIsi(String i) {
+        return isi=i;
+    }
+}

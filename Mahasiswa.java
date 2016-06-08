04d10dd6b38c7937b16771bba82f1fdef0b70160
jrsius <xave96@gmail.com>
2016-06-08 11:58:30 +0700

    Add files via upload

---
+++ Mahasiswa.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package executepart2;
+
+public class Mahasiswa {
+    String nim,nama,prodi;
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getProdi() {
+        return prodi;
+    }
+
+    public void setProdi(String prodi) {
+        this.prodi = prodi;
+    }
+    
+}
